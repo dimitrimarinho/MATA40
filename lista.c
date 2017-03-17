bb2c5e38c589b80d615093cfc416ce48d6be96a6
Gabriele Luz <gabrieleluz@infojr.com.br>
2017-03-17 12:13:00 -0300

    Inicia implementação de lista Inclui função que esvazia a lista

---
+++ lista.c
@@ -0,0 +1,32 @@
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define MAX 10
+
+/* ========================================================================= */
+
+typedef int TipoChave;
+
+typedef struct {
+  int Chave;
+  /* outros componentes */
+} TipoItem;
+
+typedef struct TipoCelula *TipoApontador;
+
+typedef struct TipoCelula {
+  TipoItem Item;
+  TipoApontador Prox;
+} TipoCelula;
+
+typedef struct {
+  TipoApontador Primeiro, Ultimo;
+} TipoLista;
+
+/* ========================================================================= */
+
+void FLVazia(TipoLista *Lista)
+{ Lista -> Primeiro = (TipoApontador) malloc(sizeof(TipoCelula));
+  Lista -> Ultimo = Lista -> Primeiro;
+  Lista -> Primeiro -> Prox = NULL;
+}
