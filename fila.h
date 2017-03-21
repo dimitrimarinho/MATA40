6af2c91f2f020f14b8a01b4db5a9efe583158026
Erickgtn <erick.gtn.xp@gmail.com>
2017-03-21 20:41:05 -0300

    Criar biblioteca fila.h

---
+++ fila.h
@@ -0,0 +1,17 @@
+typedef struct TipoCelula *TipoApontador;
+
+typedef int TipoChave;
+
+typedef struct TipoItem {
+  TipoChave Chave;
+  /* outros componentes */
+} TipoItem;
+
+typedef struct TipoCelula {
+  TipoItem Item;
+  TipoApontador Prox;
+} TipoCelula;
+
+typedef struct TipoFila {
+  TipoApontador Frente, Tras;
+} TipoFila;
