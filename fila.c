bf2ed2e7d7483b6849c19664bd91d69985cb1169
Erickgtn <erick.gtn.xp@gmail.com>
2017-03-23 15:23:35 -0300

    Inserção das bibliotecas padrões

--- fila.c
+++ fila.c
@@ -1,34 +1,42 @@
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 void FFVazia(TipoFila *Fila)
-{ Fila->Frente = (TipoApontador) malloc(sizeof(TipoCelula));
+{ 
+  Fila->Frente = (TipoApontador) malloc(sizeof(TipoCelula));
   Fila->Tras = Fila->Frente;
   Fila->Frente->Prox = NULL;
 } 
 
-int Vazia(TipoFila Fila)
-{ return (Fila.Frente == Fila.Tras); } 
+int Vazia(TipoFila Fila){ 
+  return (Fila.Frente == Fila.Tras); 
+} 
 
-void Enfileira(TipoItem x, TipoFila *Fila)
-{ Fila->Tras->Prox = (TipoApontador) malloc(sizeof(TipoCelula));
+void Enfileira(TipoItem x, TipoFila *Fila){ 
+  Fila->Tras->Prox = (TipoApontador) malloc(sizeof(TipoCelula));
   Fila->Tras = Fila->Tras->Prox;
   Fila->Tras->Item = x;
   Fila->Tras->Prox = NULL;
 } 
 
-void Desenfileira(TipoFila *Fila, TipoItem *Item)
-{ TipoApontador q;
-  if (Vazia(*Fila)) { printf("Erro fila esta vazia\n"); return; }
+void Desenfileira(TipoFila *Fila, TipoItem *Item){ 
+  TipoApontador q;
+  if (Vazia(*Fila)) { 
+    printf("Erro fila esta vazia\n"); 
+    return; 
+}
   q = Fila->Frente;
   Fila->Frente = Fila->Frente->Prox;
   *Item = Fila->Frente->Item;
   free(q);
 } 
 
-
-void Imprime(TipoFila Fila)
-{ TipoApontador Aux;
+void Imprime(TipoFila Fila){ 
+  TipoApontador Aux;
   Aux = Fila.Frente->Prox;
-  while (Aux != NULL) 
-    { printf("%d\n", Aux->Item.Chave);
-      Aux = Aux->Prox;
-    }
+  while (Aux != NULL){ 
+    printf("%d\n", Aux->Item.Chave);
+    Aux = Aux->Prox;
+  }
 }
