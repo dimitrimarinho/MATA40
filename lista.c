d98dc6a5256de249760a0ba1e00f0ffad5f7332a
Erickgtn <erick.gtn.xp@gmail.com>
2017-03-23 15:01:47 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -5,23 +5,6 @@
 
 /* ========================================================================= */
 
-typedef int TipoChave;
-
-typedef struct {
-  int Chave;
-  /* outros componentes */
-} TipoItem;
-
-typedef struct TipoCelula *TipoApontador;
-
-typedef struct TipoCelula {
-  TipoItem Item;
-  TipoApontador Prox;
-} TipoCelula;
-
-typedef struct {
-  TipoApontador Primeiro, Ultimo;
-} TipoLista;
 
 /* ========================================================================= */
 
