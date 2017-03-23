1bfc30c2fda586c3f4a3fefadf033db166953b6b
Erickgtn <erick.gtn.xp@gmail.com>
2017-03-23 14:57:21 -0300

    Criação da biblioteca.h

---
+++ lista.h
@@ -0,0 +1,17 @@
+typedef int TipoChave;
+
+typedef struct {
+  int Chave;
+  /* outros componentes */
+} TipoItem;
+
+typedef struct TipoCelula *TipoApontador;
+
+typedef struct TipoCelula {
+  TipoItem Item;
+  TipoApontador Prox;
+} TipoCelula;
+
+typedef struct {
+  TipoApontador Primeiro, Ultimo;
+} TipoLista;
