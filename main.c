9c76b8f7fbf3f5d856afb04545f9f7ec6a0b62df
Erickgtn <erick.gtn.xp@gmail.com>
2017-03-25 15:34:52 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,52 +1,169 @@
 #include <sys/time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "curses.h"
 #include "fila.c"
+#include "pilha.c"
+#include "lista.c"
 
 int main(int argc, char **argv)
-{
-	TipoItem item;
-    TipoFila fila;
-    int chave,opc;
-    
-    printf("O que deseja usar?\n\n 1-Lista\n 2-Pilha\n 3-Pilha\n 4-Arvore\n\n");
-    scanf("%d",&opc);
-    switch (opc){
-		case 3:
-		printf("FILA\n\n");
-		//printf("Escolha uma opcao: \n");
-		printf("1-Esvaziar fila \n");
-		printf("2-Inserir Elemento na fila\n ");
-		printf("3-Remover Elemento da fila \n ");
-		printf("4-Verificar se a fila esta vazia: \n");
-		
-		scanf("%d",&opc);
-		FFVazia(&fila); // incializando a pilha vazia
-		switch (opc){
-				case 1:
-					FFVazia(&fila);
-				break;
-				 
-				case 2:
-					// loop para quantos elementos deseja inserir
-					scanf("%d",&chave);
-					item.Chave = chave;
-					Enfileira(item, &fila);
-					printf ("Elemento %d Inserido\n",chave);
-				break;
-				
-				case 3:
-					Desenfileira(&fila, &item);
-					printf("Desempilhado: %d\n", item.Chave);
-				break;
-				
-				case 4:
-					printf("Em manutencao");
-				break;
-			}
-   }
-
+{ 
+	TipoFila fila;
+	TipoPilha pilha;
+	TipoLista lista;
+	
+	PilhaTipoItem Pitem;
+	FilaTipoItem Fitem;
+	ListaTipoItem Litem;
+	
+	ListaTipoApontador Lp=NULL;
    
+   
+   
+    int chave,opc=0;
+    menu:
+		printf("O que deseja usar?\n\n");
+		printf("[1] Lista\n" );
+		printf("[2] Pilha\n" );
+		printf("[3] Fila\n" );
+		printf("[4] Arvore de Busca\n" );
+		printf("[10] Documentacao\n" );
+	  
+    scanf("%d",&opc);
+
+	
+  
+    switch (opc){
+		case 1:
+		opc=0;
+			printf("LISTA\n\n");
+		
+			printf("1-Esvaziar Lista \n");
+			printf("2-Inserir Elemento na Lista\n ");
+			printf("3-Remover Elemento da Lista de uma posicao \n ");
+			printf("4-Verificar se a lista esta vazia: \n");
+			printf("5-Imprimir a lista \n");
+			printf("0-Voltar \n");
+			scanf("%d",&opc);
+			case 0:
+				goto menu;
+				break;
+		
+			
+			FLVazia(&lista); // incializando a lista vazia
+			
+			switch (opc){
+			case 1:
+				FLVazia(&lista);
+			break;
+			
+			case 2:
+				scanf("%d",&chave);
+				Litem.Chave=chave;
+				LInsere(Litem, &lista);
+			break;
+			
+			case 3:
+				LRetira(Lp, &lista, &Litem);//REVERRRRR
+			break;
+			
+			case 4:
+				if (LVazia(lista)){
+					printf("Lista Vazia\n");
+				return (0);
+				}
+				else
+					printf("Lista Nao Vazia\n");
+					return (1);	
+			break;
+			
+			case 5:
+				LImprime(lista);
+			break;
+		}
+		break;
+		case 2:
+		opc=0;
+
+			printf("PILHA\n\n");
+			printf("1-Esvaziar Pilha \n");
+			printf("2-Inserir Elemento na Pilha\n ");
+			printf("3-Remover Elemento da Pilha \n ");
+			printf("4-Verificar se a pilha esta vazia: \n");
+			
+			scanf("%d",&opc);
+			FPVazia(&pilha); // incializando a pilha vazia
+			switch (opc){
+					case 1:
+						FPVazia(&pilha);
+					break;
+					 
+					case 2:
+						scanf("%d",&chave);
+						Pitem.Chave = chave;
+						PEmpilha(Pitem, &pilha);
+						printf ("Elemento %d Inserido\n",chave);
+					break;
+					
+					case 3:
+						PDesempilha(&pilha, &Pitem);
+						printf("Desempilhado: %d\n", Pitem.Chave);
+					break;
+					
+					case 4:
+						printf("Em manutencao");
+					break;
+					
+					case 10:
+						printf("Em manutencao");
+					break;
+				}
+		break;
+		
+		
+		
+		
+	case 3:
+		opc=0;
+			printf("FILA\n\n");
+			printf("1-Esvaziar fila \n");
+			printf("2-Inserir Elemento na fila\n ");
+			printf("3-Remover Elemento da fila \n ");
+			printf("4-Verificar se a fila esta vazia: \n");
+			
+			scanf("%d",&opc);
+			FFVazia(&fila); // incializando a fila vazia
+			switch (opc){
+					case 1:
+						FFVazia(&fila);
+					break;
+					 
+					case 2:
+						scanf("%d",&chave);
+						Fitem.Chave = chave;
+						FEnfileira(Fitem, &fila);
+						printf ("Elemento %d Inserido\n",chave);
+					break;
+					
+					case 3:
+						FDesenfileira(&Fitem,&fila);
+						printf("Desempilhado: %d\n", Fitem.Chave);
+					break;
+				
+					case 4:
+						printf("Em manutencao");
+					break;
+					
+					case 10:
+						printf("Em manutencao");
+					break;
+				}
+    
+   
+	break;
+   }
+	
+
     
     /*
     item.Chave = 88;
@@ -62,6 +179,6 @@ int main(int argc, char **argv)
 
      	
 
-    Imprime(fila);
+  //  Imprime(fila);
 	return 0;
 }
