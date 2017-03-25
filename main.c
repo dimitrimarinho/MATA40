ba395f70d3c53021f9bbc7af98d0a2c25de19841
Erickgtn <erick.gtn.xp@gmail.com>
2017-03-25 18:00:59 -0300

    Update main.c

--- main.c
+++ main.c
@@ -21,23 +21,21 @@ int main(int argc, char **argv)
 	ListaTipoApontador Lp=NULL;
    
     int chave,opc=0;
-    menu:
+    menu:{
 		printf("O que deseja usar?\n\n");
 		printf("[1] Lista\n" );
 		printf("[2] Pilha\n" );
 		printf("[3] Fila\n" );
 		printf("[4] Arvore de Binaria de Busca\n" );
 		printf("[10] Documentacao\n" );
-	  
-    scanf("%d",&opc);
-
-	
+		scanf("%d",&opc);
+	}
   
     switch (opc){
 		case 1:
 		opc=0;
 			printf("LISTA\n\n");
-		
+	
 			printf("1-Esvaziar Lista \n");
 			printf("2-Inserir Elemento na Lista\n ");
 			printf("3-Remover Elemento da Lista de uma posicao \n ");
@@ -45,13 +43,14 @@ int main(int argc, char **argv)
 			printf("5-Imprimir a lista \n");
 			printf("0-Voltar \n");
 			scanf("%d",&opc);
-			case 0:
-				goto menu;
-				break;
-				
+			
 			//FLVazia(&lista); // incializando a lista vazia
 			
 			switch (opc){
+			case 0:
+				goto menu;
+			break;
+				
 			case 1:
 				FLVazia(&lista);
 			break;
@@ -89,10 +88,15 @@ int main(int argc, char **argv)
 			printf("2-Inserir Elemento na Pilha\n ");
 			printf("3-Remover Elemento da Pilha \n ");
 			printf("4-Verificar se a pilha esta vazia: \n");
-			
+			printf("0-Voltar \n");
 			scanf("%d",&opc);
+			
+				
 			FPVazia(&pilha); // incializando a pilha vazia
 			switch (opc){
+				case 0:
+					goto menu;
+				break;
 					case 1:
 						FPVazia(&pilha);
 					break;
@@ -129,10 +133,15 @@ int main(int argc, char **argv)
 			printf("2-Inserir Elemento na fila\n ");
 			printf("3-Remover Elemento da fila \n ");
 			printf("4-Verificar se a fila esta vazia: \n");
-			
+			printf("0-Voltar \n");
 			scanf("%d",&opc);
+			
+				
 			FFVazia(&fila); // incializando a fila vazia
 			switch (opc){
+				case 0:
+					goto menu;
+				break;
 					case 1:
 						FFVazia(&fila);
 					break;
@@ -156,23 +165,32 @@ int main(int argc, char **argv)
 	break;
    
 	case 4:
-	
-		opc=0;
+			Arvore:{
+			opc=0;
 			printf("ARVORE BINARIA\n\n");
 			printf("1-Iniciar uma arvore \n");
 			printf("2-Pesquisar elemento na arvore\n ");
 			printf("3-Inserir elemento na arvore\n ");
 			printf("4-Remover elemento da arvore: \n");
 			printf("5-Imprimir arvore: \n");
+			printf("0-Voltar \n");
 			scanf("%d",&opc);
-	
+			}
 		switch (opc){
+			Vazia(&arv);
+			case 0:
+				goto menu;
+			break;
 			case 1:	
 				Inicializa(&arv);
+				goto Arvore;
+				
 			break;
 			case 2:
+			
 				scanf("%d",&chave);
-				Pesquisa(&arv,chave);
+				Pesquisa(&arv,arv.chave);
+				goto Arvore;
 			break;
 			case 3:
 				scanf("%d",&chave);
@@ -181,6 +199,7 @@ int main(int argc, char **argv)
 			case 4:
 				scanf("%d",&chave);
 				Remove(&arv,chave);
+				break;
 				
 			case 5:
 				printf("Em Ordem: \n");
