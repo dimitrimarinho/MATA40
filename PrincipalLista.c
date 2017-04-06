a66ccb3f0f896fc2ad60c4d68bf44a777dff08a7
Erickgtn <erick.gtn.xp@gmail.com>
2017-04-06 15:18:56 -0300

    Update PrincipalLista.c

--- PrincipalLista.c
+++ PrincipalLista.c
@@ -1,50 +1,66 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#define true 1
+#define false 0
 
-typedef int ListaTipoChave;
+typedef int TipoChave;
 
 typedef struct {
   int Chave;
-} ListaTipoItem;
+  /* outros componentes */
+} TipoItem;
 
-typedef struct ListaTipoCelula *ListaTipoApontador;
+typedef struct TipoCelula *TipoApontador;
 
-typedef struct ListaTipoCelula {
-  ListaTipoItem Item;
-  ListaTipoApontador Prox;
-} ListaTipoCelula;
+typedef struct TipoCelula {
+  TipoItem Item;
+  TipoApontador Prox;
+} TipoCelula;
 
 typedef struct {
-  ListaTipoApontador Primeiro, Ultimo;
+  TipoApontador Primeiro, Ultimo;
 } TipoLista;
 
-
-
+/* ========================================================================= */
 
 void FLVazia(TipoLista *Lista){ 
-  Lista -> Primeiro = (ListaTipoApontador) malloc(sizeof(ListaTipoCelula));
+  Lista -> Primeiro = (TipoApontador) malloc(sizeof(TipoCelula));
   Lista -> Ultimo = Lista -> Primeiro;
   Lista -> Primeiro -> Prox = NULL;
 }
 
-int LVazia(TipoLista Lista){ 
-  return (Lista.Primeiro == Lista.Ultimo);
+int Vazia(TipoLista Lista){ 
+	if(Lista.Primeiro == Lista.Ultimo)
+		return (0);
+	else
+		return (1);
 }
 
-void LInsere(ListaTipoItem x, TipoLista *Lista){ 
-  Lista -> Ultimo -> Prox = (ListaTipoApontador) malloc(sizeof(ListaTipoCelula));
+void Insere(TipoItem x, TipoLista *Lista){ 
+  Lista -> Ultimo -> Prox = (TipoApontador) malloc(sizeof(TipoCelula));
   Lista -> Ultimo = Lista -> Ultimo -> Prox;
   Lista -> Ultimo -> Item = x;
   Lista -> Ultimo -> Prox = NULL;
-  printf("Elemento %d Inserido\n",x.Chave);
-  return;
 }
 
-void LRetira(ListaTipoApontador p, TipoLista *Lista, ListaTipoItem *Item){ /*---Obs.: o item a ser retirado e o seguinte ao apontado por  p --- */
-  ListaTipoApontador q;
-  if (LVazia(*Lista) || p == NULL || p -> Prox == NULL){ 
-    printf(" Erro:  Lista vazia ou posicao nao existe\n");
+void InsereDepois(TipoApontador p, TipoItem x, TipoLista *Lista){
+  TipoApontador aux = p->Prox;
+  TipoApontador temp = (TipoApontador) malloc(sizeof(TipoCelula));
+  p->Prox = temp;
+  temp->Prox = aux;
+  temp->Item = x;
+  if (aux==NULL){
+      Lista -> Ultimo = temp;
+      printf("ok");
+  }
+}
+
+void Retira(TipoApontador p, TipoLista *Lista, TipoItem *Item){
+ /*  ---   Obs.: o item a ser retirado e  o seguinte ao apontado por  p --- */
+  TipoApontador q;
+  if (Vazia(*Lista) || p == NULL || p -> Prox == NULL) 
+  { printf(" Erro Lista vazia ou posicao nao existe\n");
     return;
   }
   q = p -> Prox;
@@ -54,66 +70,76 @@ void LRetira(ListaTipoApontador p, TipoLista *Lista, ListaTipoItem *Item){ /*---
   free(q);
 }
 
-void LImprime(TipoLista Lista){ 
-  ListaTipoApontador Aux;
+TipoApontador Busca(TipoLista Lista, int chave){ 
+  TipoApontador Aux;
   Aux = Lista.Primeiro -> Prox;
-  while (Aux != NULL){ 
-    printf("%d\n", Aux -> Item.Chave);
+  while (Aux != NULL) { 
+	  if (Aux->Item.Chave==chave){
+	  	printf(" Achou: %d\n", Aux->Item.Chave);
+	  	return Aux;
+	  }  
       Aux = Aux -> Prox;
-  }
+    }
+	return NULL;
+}
+
+void Imprime(TipoLista Lista){ 
+  TipoApontador Aux;
+  Aux = Lista.Primeiro -> Prox;
+  while (Aux != NULL) 
+    { printf("%d\n", Aux -> Item.Chave);
+      Aux = Aux -> Prox;
+    }
 }
 
 
 
 int main(int argc, char **argv){
-	TipoLista lista;
-	ListaTipoItem Litem;
-	ListaTipoApontador Lp=NULL;
 	
-	int chave;
+	TipoItem item;
+    TipoLista lista;
+    TipoApontador p;
+	
+	int chave,a;
 	
 	int opc = 6;
 	while(opc != 0){
 
-			printf("LISTA\n\n");
+			printf("####### LISTA #######\n");
 			printf("1-Esvaziar Lista\n");
 			printf("2-Inserir Elemento na Lista\n");
-			printf("3-Remover Elemento da Lista de uma posicao\n");
-			printf("4-Verificar se a lista esta vazia\n");
+			printf("3-Remover Elemento da Lista de uma posicao (Remove o proximo)!!\n");
+			printf("4-Verificar se a lista esta vazia: 0-Vazia 1-Com elementos \n");
 			printf("5-Imprimir a lista\n");
 			printf("0-Voltar\n");
 			scanf("%d",&opc);
-			
-			//FLVazia(&lista); // incializando a lista vazia
-			
+		
 			switch (opc){
 	
 			case 1:
-				FLVazia(&lista);
+				FLVazia(&lista);// incializando a lista vazia
 			break;
 			
 			case 2:
 				scanf("%d",&chave);
-				Litem.Chave=chave;
-				LInsere(Litem, &lista);
+				item.Chave=chave;
+				Insere(item, &lista);
 			break;
 			
 			case 3:
-				LRetira(Lp, &lista, &Litem);//REVERRRRR
+				scanf("%d",&chave);
+				item.Chave=chave;
+				p = Busca(lista,chave);
+				Retira(p, &lista, &item);
 			break;
 			
 			case 4:
-				if (LVazia(lista)){
-					printf("Lista Vazia\n");
-					return (0);
-				}
-				else
-					printf("Lista Nao Vazia\n");
-					return (1);	
+				a=Vazia(lista);
+				printf("%d\n",a);
 			break;
 			
 			case 5:
-				LImprime(lista);
+				Imprime(lista);
 			break;
 		
 		break;
