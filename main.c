69df9f49ac9ae610b2dc27fd75ddb9717ffef8f5
Gabriele Luz <gabrieleluz@infojr.com.br>
2017-04-06 18:41:04 -0300

    Inclui a biblioteca

--- main.c
+++ main.c
@@ -1,61 +1,8 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <stdio.h>
+#include "library.c"
 #define MAX 10
-
-typedef struct TipoCelula *TipoApontador;
-
-typedef int TipoChave;
-
-typedef struct TipoItem {
-  TipoChave Chave;
-  /* outros componentes */
-} TipoItem;
-
-typedef struct TipoCelula {
-  TipoItem Item;
-  TipoApontador Prox;
-} TipoCelula;
-
-typedef struct TipoFila {
-  TipoApontador Frente, Tras;
-} TipoFila;
-
-void FFVazia(TipoFila *Fila){
-  Fila->Frente = (TipoApontador) malloc(sizeof(TipoCelula));
-  Fila->Tras = Fila->Frente;
-  Fila->Frente->Prox = NULL;
-} 
-
-int Vazia(TipoFila Fila){
-  return (Fila.Frente == Fila.Tras); 
-} 
-
-void Enfileira(TipoItem x, TipoFila *Fila){
-  Fila->Tras->Prox = (TipoApontador) malloc(sizeof(TipoCelula));
-  Fila->Tras = Fila->Tras->Prox;
-  Fila->Tras->Item = x;
-  Fila->Tras->Prox = NULL;
-} 
-
-void Desenfileira(TipoFila *Fila, TipoItem *Item){
-  TipoApontador q;
-  if (Vazia(*Fila)) { printf("Erro fila esta vazia\n"); return; }
-  q = Fila->Frente;
-  Fila->Frente = Fila->Frente->Prox;
-  *Item = Fila->Frente->Item;
-  free(q);
-
-} 
-
-void Imprime(TipoFila Fila){
-  TipoApontador Aux;
-  Aux = Fila.Frente->Prox;
-  while (Aux != NULL) 
-    { printf("%d\n", Aux->Item.Chave);
-      Aux = Aux->Prox;
-    }
-}
 
 
 int main(){
